4d6c5f4037e1440be9f755e58577819ba2ffd61a
vasifgumbatov <gumbatovvasif4@gmail.com>
2025-01-03 21:28:21 +0400

    bug fixed

--- app/src/main/java/com/vasifgumbatov/news/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/vasifgumbatov/news/extensions/FragmentExtensions.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 
-fun Fragment.navigateFromParent(@IdRes resId: Int, args: Bundle? = null, navOptions: NavOptions? = null){
+fun Fragment.navigateFromParent(
+    @IdRes resId: Int,
+    args: Bundle? = null,
+    navOptions: NavOptions? = null,
+) {
     parentFragment?.parentFragment?.findNavController()?.navigate(resId, args, navOptions)
 }
